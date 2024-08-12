365ca9ccab240e4e6a8cbbeceb03bcc29fb41978
Clarence Jay Fetalino <102894582+ahley18@users.noreply.github.com>
2024-08-12 10:01:37 +0800

    Create bt-controller.ino

---
+++ bt-controller.ino
@@ -0,0 +1,54 @@
+#include "BluetoothSerial.h"
+#define led 27
+
+
+BluetoothSerial serialBT;
+char cmd;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  serialBT.begin("Esp32-BT2");
+  pinMode(led,OUTPUT);
+
+}
+
+void fastBlink(){
+  digitalWrite(led,HIGH);
+  delay(100);
+  digitalWrite(led,LOW);
+  delay(100);
+}
+
+void slowBlink(){
+  digitalWrite(led,HIGH);
+  delay(1000);
+  digitalWrite(led,LOW);
+  delay(1000);
+}
+
+void loop() {
+  // Variable to store the received command
+
+  // Check if there is data available from the Bluetooth connection
+  if (serialBT.available()) {
+    cmd = serialBT.read();
+    Serial.println(cmd);
+  }
+
+  // Use a switch-case structure to handle different commands
+  switch (cmd) {
+    case 'F':  // Command
+      fastBlink();
+      break;
+
+    case 'B':  // Command
+      slowBlink();
+      break;
+
+    default:  // Default case if an unknown command is received
+      // No action or add some debugging message if needed
+      break;
+  }
+
+  delay(10);  // Small delay to avoid overwhelming the loop
+}
